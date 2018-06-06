d933f94e67e337038ca9dddcd576f59dff748b5c
owenliversidge <owen.liversidge@gmail.com>
2018-06-06 17:56:45 -0400

    added title and logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.jpg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { injectGlobal } from "styled-components";
+import styled from "styled-components";
+import logo from "./logo.jpg";
+import "./App.css";
+
+import Header from "./Header";
+import HomePage from "./components/HomePage"
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Fjalla+One');
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class Header extends Component {
+  render() {
+    return (
+      <HeaderWrapper>
+        <span className="blue">The </span>
+        <span className="red">Teacher </span>
+        <span className="black">and </span>
+        <span className="red">Student </span>
+        <span className="blue">Method</span>
+      </HeaderWrapper>
+    );
+  }
+}
+
+export default Header;
+
+const HeaderWrapper = styled.div`
+  font-family: "Fjalla One", sans-serif;
+  font-size: 48px;
+  margin-bottom: 20px;
+
+  .blue {
+    color: #459ed4;
+  }
+
+  .red {
+    color: #c33e53;
+  }
+
+  .black {
+    color: black;
+  }
+`;
